d6a9e368cb9052f4ae0284b8bf983527da41990a
Peter Jones <pjones@redhat.com>
2015-07-16 08:46:32 -0400

    Minor cleanup of error handling in dltest.
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- dltest.c
+++ dltest.c
@@ -1,22 +1,22 @@
 #include <dlfcn.h>
+#include <elfutils/libebl.h>
+#include <err.h>
+#include <fcntl.h>
 #include <gelf.h>
 #include <libelf.h>
 #include <stdio.h>
 #include <nlist.h>
-#include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <elfutils/libebl.h>
 
-typedef struct GElf_SymX
-{
+typedef struct GElf_SymX {
 	GElf_Sym sym;
 	Elf32_Word xndx;
 	char *where;
 } GElf_SymX;
 
-
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	void *h;
 	int fd;
 	Elf *elf;
@@ -31,95 +31,80 @@ int main(int argc, char *argv[]) {
 	if (argc < 2)
 		return 1;
 
-	if (!(fd = open(argv[1], O_RDONLY))) {
-		perror("open");
-		return 1;
-	}
+	if (!(fd = open(argv[1], O_RDONLY)))
+		err(1, "Could not open \"%s\"", argv[1]);
 
-	elf_version (EV_CURRENT);
+	elf_version(EV_CURRENT);
 
-	if ((elf = elf_begin(fd, ELF_C_READ_MMAP, NULL)) == NULL) {
-		printf("cannot get elf descriptor for %s: %s\n", argv[1],
-			elf_errmsg(-1));
-		return 1;
-	}
+	if ((elf = elf_begin(fd, ELF_C_READ_MMAP, NULL)) == NULL)
+		errx(1, "Cannot get elf descriptor for %s: %s", argv[1],
+		     elf_errmsg(-1));
 
 	if (elf_kind(elf) != ELF_K_ELF) {
 		elf_end(elf);
 		close(fd);
-		printf("this isn't an elf file\n");
-		return 1;
+		errx(1, "\"%s\" is not an elf file", argv[1]);
 	}
 
 	ehdr = gelf_getehdr(elf, &ehdr_mem);
 
 	if (symsec_type == SHT_DYNSYM && ehdr->e_type != ET_EXEC &&
-			ehdr->e_type != ET_DYN) {
+	    ehdr->e_type != ET_DYN) {
 		close(fd);
 		elf_end(elf);
-		printf("this isn't dynamic\n");
-		return 1;
+		errx(1, "\"%s\" is not a dynamic shared object", argv[1]);
 	}
 
 	Ebl *ebl;
 
-	if (!(ebl = ebl_openbackend(elf))) {
-		printf("couldn't open backend\n");
-		return 1;
-	}
+	if (!(ebl = ebl_openbackend(elf)))
+		errx(1, "Could not open ebl backend");
 
 	while ((scn = elf_nextscn(elf, scn)) != NULL) {
 		GElf_Shdr shdr_mem;
-		GElf_Shdr *shdr = gelf_getshdr (scn, &shdr_mem);
+		GElf_Shdr *shdr = gelf_getshdr(scn, &shdr_mem);
 		size_t shstrndx;
 
 		if (symsec_type != shdr->sh_type)
 			continue;
 
-		if (elf_getshdrstrndx (elf, &shstrndx) < 0) {
-			printf("elf_getshstrndx() failed\n");
-			return 1;
-		}
+		if (elf_getshdrstrndx(elf, &shstrndx) < 0)
+			errx(1, "elf_getshstrndx() failed");
 
 		size_t size = shdr->sh_size;
 		size_t entsize = shdr->sh_entsize;
-		size_t nentries = size / (entsize ?: 1);
+		size_t nentries = size / (entsize ? : 1);
 
-		if (entsize != gelf_fsize (elf, ELF_T_SYM, 1, ehdr->e_version)) {
-			printf("elf is malformed\n");
-			return 1;
-		}
-
+		if (entsize != gelf_fsize(elf, ELF_T_SYM, 1, ehdr->e_version))
+			errx(1, "ELF is malformed.");
 
 		GElf_SymX *sym_mem;
 
-		sym_mem = (GElf_SymX *) malloc(nentries * sizeof (GElf_SymX));
-		Elf_Data *data = elf_getdata (scn, NULL);
-		if (data == NULL) {
-			printf("elf_getdata failed\n");
-			return 1;
-		}
+		sym_mem = (GElf_SymX *) malloc(nentries * sizeof(GElf_SymX));
+		Elf_Data *data = elf_getdata(scn, NULL);
+		if (data == NULL)
+			errx(1, "elf_getdata() failed");
 
 		size_t nentries_used = 0;
 		for (size_t cnt = 0; cnt < nentries; ++cnt) {
 			GElf_Sym *sym = gelf_getsymshndx(data, NULL, cnt,
-				&sym_mem[nentries_used].sym,
-				&sym_mem[nentries_used].xndx);
+							 &sym_mem
+							 [nentries_used].sym,
+							 &sym_mem
+							 [nentries_used].xndx);
 
-			if (sym == NULL) {
-				printf("sym was NULL\n");
-				return 1;
-			}
+			if (sym == NULL)
+				errx(1, "sym was NULL");
 
 			if (sym->st_shndx != SHN_UNDEF)
 				continue;
 
 			const char *symstr = elf_strptr(elf, shdr->sh_link,
-					sym->st_name);
+							sym->st_name);
 			if (symstr == NULL || symstr[0] == '\0')
 				continue;
 
-			nentries_used ++;
+			nentries_used++;
 
 			printf("symbol is %s\n", symstr);
 		}
@@ -136,15 +121,12 @@ int main(int argc, char *argv[]) {
 		if (symbols[i].n_name == NULL)
 			break;
 		printf("%s %ld %d %d\n", symbols[i].n_name, symbols[i].n_value,
-			symbols[i].n_type, symbols[i].n_sclass);
+		       symbols[i].n_type, symbols[i].n_sclass);
 	}
-
 
 	h = dlopen(argv[1], RTLD_LAZY);
-	if (!h) {
-		printf("cannot open '%s': %s\n", argv[1], dlerror());
-		return 1;
-	}
+	if (!h)
+		errx(1, "cannot open '%s': %s", argv[1], dlerror());
 
 	elf_end(elf);
 	return 0;
