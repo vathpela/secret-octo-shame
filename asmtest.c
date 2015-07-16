e0ca82110549bf9247eb0ee2b06730d1154eefb7
Peter Jones <pjones@redhat.com>
2015-07-16 08:49:27 -0400

    Very very minor cleanup of asmtest.c
    Doesn't really do much...
    
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- asmtest.c
+++ asmtest.c
@@ -1,38 +1,31 @@
+#include <err.h>
 #include <stdio.h>
 
 #include "libso.h"
 
 static GElf_Half get_elf_machine(void)
 {
-    return EM_386;
+	return EM_386;
 }
 
 int main(int argc, char *argv[])
 {
-    SoCtx_t *ctx;
+	SoCtx_t *ctx;
 
-    if (argc < 2) {
-        fprintf(stderr, "no output file specified\n");
-        return 1;
-    }
+	if (argc < 2)
+		errx(1, "No ouptut file specified");
 
-    GElf_Half machine;
-    if ((machine = get_elf_machine()) == EM_NONE) {
-        fprintf(stderr, "could not detect elf machine\n");
-        return 1;
-    }
+	GElf_Half machine;
+	if ((machine = get_elf_machine()) == EM_NONE)
+		errx(1, "Could not detect ELF machine type");
 
-    if (!(ctx = so_begin(argv[1], machine))) {
-        fprintf(stderr, "cannot create so context\n");
-        return 1;
-    }
+	if (!(ctx = so_begin(argv[1], machine)))
+		errx(1, "Could not create context for DSO");
 
-    so_end(ctx);
-    return 0;
+	so_end(ctx);
+	return 0;
 }
 
-
-
 /*
- * vim:ts=8:sw=4:sts=4:et
+ * vim:ts=8:sw=8:sts=8:et
  */
